2a45388c17534837d60f22ed9d959310b68cbf52
Daniel <heyboy6059@gmail.com>
2019-08-27 00:37:29 +1000

    Build layout and add more contents in landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render(){
         return (
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/malecostume-512.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+                            <hr/>
+                            <p>
+                                HTML/CSS | JavaScript | React | Python
+                            </p>
+                        </div>
+
+                    </Cell>
+                </Grid>
             </div>
         );
     }
